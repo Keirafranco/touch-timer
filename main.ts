9e2a7e4be8bbdeb11ff1113ebfc090831a852b00
Keirafranco <125355714+Keirafranco@users.noreply.github.com>
2023-03-07 10:22:08 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,19 @@
-let time = 0
 let start = 0
+let time = 0
+input.onLogoEvent(TouchButtonEvent.Touched, function () {
+    start = input.runningTime()
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        # . # . #
+        . # . # .
+        . . # . .
+        `)
+})
 input.onLogoEvent(TouchButtonEvent.Released, function () {
     time = input.runningTime() - start
     basic.showNumber(Math.idiv(time, 1000))
-})
-input.onLogoEvent(TouchButtonEvent.Touched, function () {
-    start = input.runningTime()
-    basic.showIcon(IconNames.Heart)
+    basic.pause(100)
+    basic.clearScreen()
+    basic.showNumber(time % 1000)
 })
